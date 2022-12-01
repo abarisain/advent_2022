c1579e4967012a1f6a634755de164bb15ea0ead3
Arnaud Barisain-Monrose <abarisain@gmail.com>
2022-12-01 11:55:59 +0100

    day 1

---
+++ day_01/Sources/day_01/day_01.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+let sourceDataURL = URL(string: "file:///Users/arnaud/dev/github/abarisain/advent_2022/day_01/input.txt")!
+
+@available(macOS 13.0, *)
+@main
+public struct day_01 {
+    public private(set) var text = "Hello, World!"
+
+    @available(macOS 13.0, *)
+    public static func main() {
+        let input = try! String(contentsOf: sourceDataURL, encoding: .ascii)
+        let splitted = input.split(separator: "\n", omittingEmptySubsequences: false)
+        
+        var initialElves = [[Int]]()
+        initialElves.append([])
+        
+        let grouped = splitted.reduce(initialElves) { partialResult, value in
+            var elves = partialResult
+            if (value == "") {
+                elves.append([])
+            } else {
+                var carrying = elves.removeLast()
+                carrying.append(Int(value)!)
+                elves.append(carrying)
+            }
+            return elves
+        }
+        
+        let reduced = grouped.reduce([Int]()) { partialResult, carrying in
+            var elves = partialResult
+            elves.append(carrying.reduce(0) { x, y in
+                x + y
+            })
+            return elves
+        }
+        
+        let max = reduced.sorted(by: >).first!
+        
+        print("Done: \(max)")
+    }
+}
---
+++ day_01/Tests/day_01Tests/day_01Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import day_01
+
+final class day_01Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(day_01().text, "Hello, World!")
+    }
+}
