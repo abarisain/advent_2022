35004c90bee11586139bf9038e034d8f5ca1420b
Arnaud Barisain-Monrose <abarisain@gmail.com>
2022-12-04 08:34:43 +0100

    day 4

--- day04/Sources/day-1.swift
+++ day04/Sources/day-1.swift
@@ -1,9 +1,39 @@
 import Foundation
 
+func parseRange(_ input: Substring) -> CountableClosedRange<Int> {
+    let split = input.split(separator: "-")
+    let left = Int(split[0])!
+    let right = Int(split[1])!
+    return left...right
+}
+
+struct Pair {
+    let first: CountableClosedRange<Int>
+    let second: CountableClosedRange<Int>
+    
+    var hasCompleteOverlap: Bool {
+        get {
+            let firstSet = Set(first)
+            let secondSet = Set(second)
+            return firstSet.isSubset(of: secondSet) || secondSet.isSubset(of: firstSet)
+        }
+    }
+    
+    init(input: Substring) {
+        let split = input.split(separator: ",")
+        self.first = parseRange(split[0])
+        self.second = parseRange(split[1])
+    }
+}
+
 func run1() {
     print("1:")
     
+    let input = input.split(separator: "\n")
+    let pairs = input.map { Pair(input: $0) }
+    let overlapping = pairs.map { $0.hasCompleteOverlap }.filter { $0 == true }
+    let sum = overlapping.count
     
-    
+    print("Sum: \(sum)")
     print("Done")
 }
--- day04/Sources/day-2.swift
+++ day04/Sources/day-2.swift
@@ -1,9 +1,23 @@
 import Foundation
 
+extension Pair {
+    var hasOverlap: Bool {
+        get {
+            let firstSet = Set(first)
+            let secondSet = Set(second)
+            return firstSet.intersection(secondSet).count > 0
+        }
+    }
+}
+
 func run2() {
     print("2:")
     
+    let input = input.split(separator: "\n")
+    let pairs = input.map { Pair(input: $0) }
+    let overlapping = pairs.map { $0.hasOverlap }.filter { $0 == true }
+    let sum = overlapping.count
     
-    
+    print("Sum: \(sum)")
     print("Done")
 }
