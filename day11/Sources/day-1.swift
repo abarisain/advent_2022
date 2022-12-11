0909fd855d6fd0a79d732f912e9aaa2d53dafd1f
Arnaud Barisain-Monrose <abarisain@gmail.com>
2022-12-11 14:16:44 +0100

    day 11 part 1

---
+++ day11/Sources/day-1.swift
@@ -0,0 +1,198 @@
+import Foundation
+import Collections
+
+enum Operand {
+    case old
+    case value(Int)
+}
+
+struct Operation {
+    typealias Operator = (Int, Int) -> Int
+    
+    let leftOperand: Operand
+    let rightOperand: Operand
+    let operation: Operator
+    
+    init(_ left: Operand, _ operation: @escaping Operator, _ right: Operand) {
+        self.leftOperand = left
+        self.rightOperand = right
+        self.operation = operation
+    }
+    
+    func perform(old: Int) -> Int {
+        let left = unwrapOperand(leftOperand, old: old)
+        let right = unwrapOperand(rightOperand, old: old)
+        return operation(left, right)
+    }
+    
+    private func unwrapOperand(_ operand: Operand, old: Int) -> Int {
+        switch operand {
+            case .old:
+                return old
+            case .value(let value):
+                return value
+        }
+    }
+}
+
+struct Test {
+    let value: Int
+    
+    func test(against: Int) -> Bool {
+        return against % value == 0
+    }
+}
+
+typealias MonkeyID = Int
+
+struct Monkey {
+    typealias Item = Int
+    
+    var items: Deque<Item>
+    var operation: Operation
+    var test: Test
+    var targetMonkeyIfTrue: MonkeyID
+    var targetMonkeyIfFalse: MonkeyID
+    
+    var inspectedItems = 0
+    
+    var isTurnFinished: Bool {
+        return items.isEmpty
+    }
+    
+    // Performs the turn and returns what item to throw to whom
+    mutating func performTurnStep() -> (MonkeyID, Item) {
+        if items.isEmpty {
+            fatalError("Monkey cannot perform turn step with no item")
+        }
+        let item = items.removeFirst()
+        inspectedItems += 1
+        
+        var worry: Int = item
+        worry = operation.perform(old: worry)
+        
+        // Monkey gets bored
+        // No need to floor: int division returns a floored int
+        worry = worry / 3
+        
+        let monkeyToThrowTo: MonkeyID
+        if test.test(against: worry) {
+            monkeyToThrowTo = targetMonkeyIfTrue
+        } else {
+            monkeyToThrowTo = targetMonkeyIfFalse
+        }
+        
+        return (monkeyToThrowTo, worry)
+    }
+    
+    mutating func take(item: Item) {
+        items.append(item)
+    }
+}
+
+//MARK: Parsers
+
+extension Test {
+    static func from(_ input: Substring) -> Test {
+        let split = input.split(separator: "by ")
+        return Test(value: Int(split[1])!)
+    }
+}
+
+extension Operand {
+    static func from(_ input: Substring) -> Operand {
+        if input == "old" {
+            return .old
+        }
+        return .value(Int(input)!)
+    }
+}
+
+extension Operation {
+    static func operatorFrom(_ input: Substring) -> Operator {
+        switch input {
+            case "*":
+                return (*)
+            case "+":
+                return (+)
+            case "-":
+                return (-)
+            default:
+                fatalError("Unknown operator \(input)")
+        }
+    }
+}
+
+func parseTest(_ rawTest: [Substring]) -> (Test, MonkeyID, MonkeyID) {
+    let test = Test.from(rawTest[0])
+    let trueTarget = MonkeyID(rawTest[1].split(separator: " ").last!)!
+    let falseTarget = MonkeyID(rawTest[2].split(separator: " ").last!)!
+    
+    return (test, trueTarget, falseTarget)
+}
+
+func parseOperation(_ rawOperation: Substring) -> Operation {
+    var split = rawOperation.split(separator: " ")
+    
+    let rightOperand = Operand.from(split.removeLast())
+    let parsedOperator = Operation.operatorFrom(split.removeLast())
+    let leftOperand = Operand.from(split.removeLast())
+    
+    return Operation(leftOperand, parsedOperator, rightOperand)
+}
+
+func parseMonkey(_ rawMonkey: Substring) -> Monkey {
+    let split = rawMonkey.split(separator: "\n")
+    
+    let items = split[1].split(separator: ": ")[1]
+        .split(separator: ", ")
+        .map { Monkey.Item($0)! }
+    
+    let operation = parseOperation(split[2])
+    let (test, trueTarget, falseTarget) = parseTest(Array(split[3...5]))
+    
+    return Monkey(items: Deque(items),
+                  operation: operation,
+                  test: test,
+                  targetMonkeyIfTrue: trueTarget,
+                  targetMonkeyIfFalse: falseTarget)
+}
+
+func parseMonkeys() -> [Monkey] {
+    // Assume that the monkeys in the input are ordered
+    let split = input.split(separator: "\n\n")
+    return split.map { parseMonkey($0) }
+}
+
+func run1() {
+    useSampleInput = false
+    print("1:")
+    
+    var monkeys = parseMonkeys()
+    
+    for round in 1...20 {
+        print("Round n°\(round)")
+        monkeys.indices.forEach { i in
+            var monkey = monkeys[i]
+            while !monkey.isTurnFinished {
+                let (targetMonkey, item) = monkey.performTurnStep()
+                monkeys[targetMonkey].take(item: item)
+            }
+            monkeys[i] = monkey
+        }
+    }
+    
+    let inspectedItems = monkeys.map { $0.inspectedItems }
+    
+    inspectedItems.indices.forEach { i in
+        print("Monkey \(i) inspected items \(monkeys[i].inspectedItems) times")
+    }
+    
+    let sorted = inspectedItems.sorted(by: >)
+    let first = sorted[0]
+    let second = sorted[1]
+    let answer = first * second
+    print("Anwser: \(answer)")
+    
+    print("Done")
+}
